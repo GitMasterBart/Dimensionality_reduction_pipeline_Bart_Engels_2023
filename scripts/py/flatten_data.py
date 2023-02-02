daeb0b3fcb3176d96b670a2f7eadab833743d77f
GitMasterBart <bartengels15@live.nl>
2023-02-02 11:52:19 +0100

    adding parser for flattend data

--- scripts/py/flatten_data.py
+++ scripts/py/flatten_data.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import pandas
-import openpyxl
+import file_names
 
 
 def create_summed_data_matrix(boolean_temporal_df):
@@ -18,24 +18,12 @@ def create_summed_data_matrix(boolean_temporal_df):
         # print(df)
         append_data.append(df_new)
     append_data = pandas.concat(append_data, axis=1, sort=True)
-    append_data = append_data.drop(["Year"])
-    append_data = append_data.drop(["Year"])
+    # append_data = append_data.drop(["sum"])
+
+    # append_data = append_data.drop(["Year"])
     #append_data = append_data.apply(lambda x: x + 1)
-    append_data.to_csv('clinical_trajectories_dictionary_22_12_22.csv')
+    append_data.to_csv(file_names.OUTPUTFILE_FLATTEND_DATA + 'flattend_data_02_02_23.csv')
 
-
-def create_temporal_data_matrix(boolean_temporal_df):
-    df_t_s = []
-    df = pandas.read_pickle(boolean_temporal_df)
-    for donor, values in df.items():
-        df_t = pandas.DataFrame(values)
-        df_t_s.append(df_t)
-    df_t_s = pandas.concat(df_t_s, axis=1, sort=True)
-    df_t_s = df_t_s.groupby(df_t_s.columns, axis=1).sum()
-    df_t_s = df_t_s.drop(["Year"])
-    df_t_s = df_t_s.drop([-9.0, -1.0], axis=1)
-    df_t_s = df_t_s.apply(lambda x: x + 1)
-    df_t_s.to_csv('clinical_trajectories_dictionary_22_12_22.csv')
 
 def open_pickl(pkl):
     with open(pkl, 'rb') as f:
@@ -43,7 +31,7 @@ def open_pickl(pkl):
     return boolean_temporal_dfs
 
 def main():
-    picke_file3 = '../data_umcg/pickle_files/ALL_clinical_trajectories_dictionary_rules_of_thumb_2022-12-15.pkl'
+    picke_file3 = file_names.PICKLE_FILE
     #
     #print(open_pickl(picke_file1))
     #print(open_pickl(picke_file2))
@@ -53,7 +41,6 @@ def main():
     #df = pandas.read_pickle(boolean_temporal_dfs)
 
     df_t = pandas.DataFrame(boolean_temporal_dfs)
-    df_t.to_csv("ou.csv")
     sum_boolean_temporal_dfs = {donor: pandas.DataFrame(values) * True for donor, values in boolean_temporal_dfs.items()}
     #print(sum_boolean_temporal_dfs)
    # create_temporal_data_matrix(sum_boolean_temporal_dfs)
