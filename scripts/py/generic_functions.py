555f76f564b3ea45a43270429af84c45fe5dd24e
GitMasterBart <bartengels15@live.nl>
2023-02-23 16:55:15 +0100

    addition of comments

--- scripts/py/generic_functions.py
+++ scripts/py/generic_functions.py
@@ -1,29 +1,24 @@
 #!usr/bin/env python3
 
-import sys
+
 import ast
-import pandas as pd
 import pickle
+import pandas as pd
 import file_names
-import converd_pickle_to_temporalData
 
 
-def to_hierarchical(df, column):
+def to_hierarchical(data_frame, column):
     """
     This function is used to convert a dataframe to a hierarchical dictionary.
-    :param df: The dataframe to convert
+    :param data_frame: The dataframe to convert
     :param column: The column to use as the key
     :return: A hierarchical dictionary
     """
-
-    domains = df["Domain"].unique()
-
-    domains_dict = {x: {} for x in domains}
-
+    domains = data_frame["Domain"].unique()
+    domains_dict = {domain: {} for domain in domains}
     # Change the 'ç' to 'c' in the column Parameter
-    df["Parameter"] = df["Parameter"].str.replace("ç", "c")
-
-    attributes = df[[column, "Grouping", "Domain", "Parameter"]].values.tolist()
+    data_frame["Parameter"] = data_frame["Parameter"].str.replace("ç", "c")
+    attributes = data_frame[[column, "Grouping", "Domain", "Parameter"]].values.tolist()
     for group in attributes:
         current_domain = group[2]
         current_grouping = group[1]
@@ -31,7 +26,6 @@ def to_hierarchical(df, column):
         selected_domain = domains_dict[current_domain]
         if current_grouping in selected_domain:
             selected_domain[current_grouping].append(attribute)
-
         else:
             selected_domain[current_grouping] = [attribute]
 
@@ -40,12 +34,14 @@ def to_hierarchical(df, column):
 
 def select_domains(domain_list):
     """
-
+    Domain list: that includes all the domains of interest
     Args:
-        domain_list:
-
+        domain_list: a list with domains form who the
+        signs and symptoms needs to be included in the rest of the analysis.
     Returns:
-
+        A filtered dataframe, with
+        only the sign and symptoms
+        that are part of the given domains
     """
     df_attributes = pd.read_excel(file_names.ATTRIBUTE_GROUPING, engine="openpyxl")
     hierarchy = to_hierarchical(df_attributes, "Distinct_attribute")
@@ -56,28 +52,29 @@ def select_domains(domain_list):
             symp_list += domain_hierachy.get(sign_symp)
     return symp_list
 
-
-def optain_dataframe(boolean_temporal_dfs):
-    """Obtain data frame from the loaded pickle file
-        Args:
-        boolean_temporal_dfs (dict): a dictionary of dataframes
-        Returns:
-        pandas.DataFrame: a data frame obtained from the dictionary
-        """
-    FILE_LOCATION = (
-        "/Users/bengels/Desktop/stage_umcg2022/scripts/Dimensionality_reduction_pipeline_Bart_Engels_2023/Input/Clinical History - attributes grouping in categories - metadata.xlsx")
-    df_attributes = pd.read_excel(FILE_LOCATION, engine="openpyxl")
-    return df_attributes
-
-
 def open_pickle(file):
+    """
+    open pickle file
+    Args:
+        file: name + path pickle file
+    Returns:
+        open pickle file
+    """
     picke_file = file
-    with open(picke_file, 'rb') as f:
-        boolean_temporal_dfs = pickle.load(f)
+    with open(picke_file, 'rb') as file:
+        boolean_temporal_dfs = pickle.load(file)
     return boolean_temporal_dfs
 
 
 def remove_empty_values(symptom_dict):
+    """
+    removes empty values from the dict
+    Args:
+        symptom_dict: donor dict
+    Returns:
+        symptom dict without empty files
+
+    """
     # Remove the empty values from the dictionary
     for key, value in dict(symptom_dict).items():
         if value is None:
@@ -85,20 +82,20 @@ def remove_empty_values(symptom_dict):
     return symptom_dict
 
 
-def change_nan_to_0(df):
+def change_nan_to_0(data_frame):
     """
         Change NAN values in dataframe to 0
 
         Arguments:
-        df - dataframe
+        data_frame - dataframe
 
         Returns:
-        df - dataframe with NAN values changed to 0
+        data_frame - dataframe with NAN values changed to 0
         """
-    return df.fillna(0)
+    return data_frame.fillna(0)
 
 
-def clean_values(x):
+def clean_values(string_list):
     """Convert string representation of list into a list
 
     Args:
@@ -107,7 +104,7 @@ def clean_values(x):
     Returns:
     list: list representation of the string
     """
-    return ast.literal_eval(x)[0]
+    return ast.literal_eval(string_list)[0]
 
 
 def merge_df_gen_df_clinical(dfgen, dfclinical):
@@ -129,7 +126,15 @@ def merge_df_gen_df_clinical(dfgen, dfclinical):
 
 
 def check_csv(file_name):
+    """
+    checks if csv has the correct extension
+    Args:
+        file_name: a string, that represents a
+        csv-file name
+    Returns:
+        the file name if the extension is correct,
+        else it returns an error message.
+    """
     if not file_name.endswith(".csv"):
-        Exception("Error: No csv extension")
-    else:
-        return file_name
+        raise Exception("Error: No csv extension")
+    return file_name
