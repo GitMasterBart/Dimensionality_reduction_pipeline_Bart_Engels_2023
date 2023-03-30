2eaf54aa326e11ab2f5e971fd97c4e2c2b6db01a
GitMasterBart <bartengels15@live.nl>
2023-03-30 15:38:31 +0200

    adding filepath files

--- scripts/r/file_names.R
+++ scripts/r/file_names.R
@@ -1,4 +1,4 @@
 SYMPTOMFILE <- "/Input/meta_data/flattened_data_thumbRules.csv"
 TEMPORAL_FILE <- "/Input/temporal_data/temporaldata_birth_1year_al.csv"
-ROOT_FOLDER <- "/Users/bengels/Desktop/stage_umcg2022/scripts/Dimensionality_reduction_pipeline_Bart_Engels_2023"
+ROOT_FOLDER <- "[PATH TO ROOT]"
 SUMMED_FILE <- "/Input/flattened_data/flattened_data_thumbRules.csv"
